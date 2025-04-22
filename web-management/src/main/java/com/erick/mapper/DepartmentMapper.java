eb00625debedfdf52dd64c0f00375872421d6bc1
Erick Chen <112253601+zewenchenerick@users.noreply.github.com>
2025-04-22 21:42:53 +1000

    fix(DepartmentMapper): Result encapsulation for create time and update time

--- web-management/src/main/java/com/erick/mapper/DepartmentMapper.java
+++ web-management/src/main/java/com/erick/mapper/DepartmentMapper.java
@@ -13,6 +13,11 @@ public interface DepartmentMapper {
      * Query all department data
      * @return list of departments
      */
+    /*@Results({
+            @Result(column = "create_time", property = "createTime"),
+            @Result(column = "update_time", property = "updateTime")
+    })*/
+    // @Select("select id, name, create_time as createTime, update_time as updateTime from dept order by update_time desc")
     @Select("select id, name, create_time, update_time from dept order by update_time desc")
     List<Dept> findAll();
 
